95ec9e67a75b3f1e413e0d4ce652e51ee324d409
rrichy <rasulrichy@gmail.com>
2021-05-06 09:20:48 +0800

    final

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,22 @@
-def arithmetic_arranger(problems):
+def arithmetic_arranger(problems, answer = False):
+    if len(problems) > 5: return "Error: Too many problems."
+    operand_1, operand_2, dash, answers = [], [], [], []
+    for problem in problems:
+        num1, op, num2 = problem.split()
+        test1, test2 = 0, 0
+        if op != "+" and op != "-": return "Error: Operator must be '+' or '-'."
+        try:
+            test1 = int(num1)
+            test2 = int(num2)
+        except:
+            return "Error: Numbers must only contain digits."
+        if test1 > 9999 or test2 > 9999 or test1 < 0 or test2 < 0: return "Error: Numbers cannot be more than four digits."
 
-
-    return arranged_problems
+        width = len(str(max(test1, test2)))
+        operand_1.append("  " + (num1 if len(num1) == width else " "*(width - len(num1)) + num1))
+        operand_2.append(op + " " + (num2 if len(num2) == width else " "*(width - len(num2)) + num2))
+        dash.append("-"*(width + 2))
+        if answer:
+            sol = str(test1 + test2 if op == '+' else test1 - test2)
+            answers.append(" "*(width + 2 - len(sol)) + sol)
+    return "    ".join(operand_1) + "\n" + "    ".join(operand_2) + "\n" + "    ".join(dash) + ("\n" + "    ".join(answers) if answer == True else "")
--- main.py
+++ main.py
@@ -5,6 +5,5 @@ from unittest import main
 
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
 
-
 # Run unit tests automatically
 main(module='test_module', exit=False)
